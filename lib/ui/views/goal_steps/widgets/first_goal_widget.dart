79eac0a48b07bd3927a0b53d76ca630138cff987
Edmond Pruteanu <edmondpr@gmail.com>
2021-08-02 18:57:26 +0300

    Added keystore properties in build.gradle

--- lib/ui/views/goal_steps/widgets/first_goal_widget.dart
+++ lib/ui/views/goal_steps/widgets/first_goal_widget.dart
@@ -120,7 +120,7 @@ class _FirstGoalState extends State<FirstGoal> {
                     padding: const EdgeInsets.all(20.0),
                     child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                        primary: AppColors.MONZA,
+                        primary: AppColors.WATERCOURSE,
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(20.0)
                         ),
